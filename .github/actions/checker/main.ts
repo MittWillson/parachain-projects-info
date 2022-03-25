fb7c29d933fcab54eccc98cbe396125d5e6da758
Mitt <10653144+MittWillson@users.noreply.github.com>
2022-03-25 13:51:28 +0800

    feat: sign verify checker

--- .github/actions/checker/main.ts
+++ .github/actions/checker/main.ts
@@ -1,5 +1,5 @@
-const { cryptoWaitReady, decodeAddress, signatureVerify} = require('@polkadot/util-crypto');
-const { u8aToHex } = require('@polkadot/util');
+const {cryptoWaitReady, decodeAddress, signatureVerify} = require('@polkadot/util-crypto');
+const {u8aToHex} = require('@polkadot/util');
 const actions = require('@actions/core')
 const fs = require('fs')
 
@@ -10,58 +10,44 @@ const isValidSignature = (signedMessage, signature, address) => {
     return signatureVerify(signedMessage, signature, hexPublicKey).isValid;
 };
 
+const re = /The content signature is `([a-zA-Z\d ]+)` with account `([a-zA-Z\d ])`/
+
 const main = async () => {
-    await cryptoWaitReady();
-
-    // const changes: string[] = JSON.parse(actions.getInput("filenames"))
-    const changes: string[] = ['networks/polkadot/astar.json']
-    for (const change of changes) {
-        // check signature
-        if (change.startsWith('networks/')) {
-            const body = fs.readFileSync(change, 'utf8')
-            const object: {
-                data: [string, any],
-                signature: string
-            } = JSON.parse(body)
-
-            const owner: string = object.data["Owner account"]
-            const signature = object.signature
-
-            const dataKey = `"data":`
-            const startPos = body.indexOf(dataKey) + dataKey.length + 1
-            let endPos = -1
-
-            let strCount = 0
-            let markCount = 0
-            for (let i = startPos; i < body.length; i++) {
-                if (body[i] === '"') {
-                    if (strCount > 0) {
-                        strCount--
-                    } else {
-                        strCount++
-                    }
-                } else if (strCount === 0 && body[i] === '{') {
-                    markCount++
-                } else if (strCount === 0 && body[i] === '}') {
-                    markCount--
-                    if (markCount === 0) {
-                        endPos = i + 1
-                        break
-                    }
-                }
-            }
-
-            const text = body.substring(startPos, endPos)
-
-            console.log(text, signature, owner)
-
-            if (!isValidSignature(text, signature, owner)) {
-                actions.setFailed("the signature is not valid")
-                break
-            }
-        }
+    const prContent: string = actions.getInput('prContent')
+    const changes: string[] = actions.getInput('filenames').split('\r')
+    // const changes: string[] = ['networks/polkadot/astar.json']
+    if (changes.length === 0) {
+        return
     }
-    // true
+
+    const result = re.exec(prContent)
+    if (result.length !== 3) {
+        actions.setFailed('the PR Content is not expected')
+        return
+    }
+
+    const signature = result[1]
+    const owner = result[2]
+
+
+    await cryptoWaitReady();
+    for (const change of changes) {
+        if (!change.startsWith('networks/')) {
+            continue
+        }
+
+        // check signature
+        const body = fs.readFileSync(change, 'utf8')
+        if (body.length === 0) {
+            continue
+        }
+
+        if (!isValidSignature(body, signature, owner)) {
+            actions.setFailed("the signature is not valid")
+        }
+
+        break
+    }
 }
 
-main();
+main().then();
