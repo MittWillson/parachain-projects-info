0de2dbf26aa1a73df691ebad11ac9e8ff27b5c5e
Mitt <10653144+MittWillson@users.noreply.github.com>
2022-03-25 13:51:28 +0800

    feat: merge trigger and sign verifier

--- .github/actions/checker/main.ts
+++ .github/actions/checker/main.ts
@@ -1,6 +1,9 @@
-const { cryptoWaitReady, decodeAddress, signatureVerify} = require('@polkadot/util-crypto');
-const { u8aToHex } = require('@polkadot/util');
+const {cryptoWaitReady, decodeAddress, signatureVerify} = require('@polkadot/util-crypto');
+const {u8aToHex} = require('@polkadot/util');
 const actions = require('@actions/core')
+
+const {getOctokit, context} = require('@actions/github')
+
 const fs = require('fs')
 
 const isValidSignature = (signedMessage, signature, address) => {
@@ -10,58 +13,75 @@ const isValidSignature = (signedMessage, signature, address) => {
     return signatureVerify(signedMessage, signature, hexPublicKey).isValid;
 };
 
-const main = async () => {
-    await cryptoWaitReady();
-
-    // const changes: string[] = JSON.parse(actions.getInput("filenames"))
-    const changes: string[] = ['networks/polkadot/astar.json']
-    for (const change of changes) {
-        // check signature
-        if (change.startsWith('networks/')) {
-            const body = fs.readFileSync(change, 'utf8')
-            const object: {
-                data: [string, any],
-                signature: string
-            } = JSON.parse(body)
-
-            const owner: string = object.data["Owner account"]
-            const signature = object.signature
-
-            const dataKey = `"data":`
-            const startPos = body.indexOf(dataKey) + dataKey.length + 1
-            let endPos = -1
-
-            let strCount = 0
-            let markCount = 0
-            for (let i = startPos; i < body.length; i++) {
-                if (body[i] === '"') {
-                    if (strCount > 0) {
-                        strCount--
-                    } else {
-                        strCount++
-                    }
-                } else if (strCount === 0 && body[i] === '{') {
-                    markCount++
-                } else if (strCount === 0 && body[i] === '}') {
-                    markCount--
-                    if (markCount === 0) {
-                        endPos = i + 1
-                        break
-                    }
-                }
-            }
-
-            const text = body.substring(startPos, endPos)
-
-            console.log(text, signature, owner)
-
-            if (!isValidSignature(text, signature, owner)) {
-                actions.setFailed("the signature is not valid")
-                break
-            }
-        }
+const getPRContent = async (token: string, sha: string) => {
+    const octKit = getOctokit(token)
+    const result = await octKit.rest.repos.listPullRequestsAssociatedWithCommit({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        commit_sha: sha,
+    });
+    const pullRequests = result.data.filter((pullRequest) => pullRequest.state === 'open');
+    if (pullRequests.length === 0) {
+        return null
     }
-    // true
+
+    let pr = pullRequests.length > 0 && pullRequests[0];
+    pullRequests.forEach(pullRequest => pullRequest.head.sha.startsWith(sha) && (pr = pullRequest));
+    return pr
 }
 
-main();
+const re = /The content signature is `([a-zA-Z\d ]+)` with account `([a-zA-Z\d ]+)`/
+
+const main = async () => {
+    const changes: string[] = actions.getInput('filenames').split(' ')
+    // const changes: string[] = ['networks/westend/test.json']
+    if (changes.length === 0) {
+        return
+    }
+
+    const githubToken = actions.getInput('token')
+    const sha = actions.getInput('sha')
+
+    const pr = await getPRContent(githubToken, sha)
+
+    const result = re.exec(pr.body)
+    if (!result || result.length !== 3) {
+        actions.setFailed('the PR Content is not expected')
+        return
+    }
+
+    const signature = result[1].trim()
+    const owner = result[2].trim()
+
+    console.log('result', signature, owner)
+
+    let verified = true
+
+    await cryptoWaitReady();
+    for (const file of changes) {
+        if (!file.startsWith('networks/') || !file.endsWith('.json')) {
+            continue
+        }
+
+        console.log('filename', file)
+
+        // check signature
+        const body = fs.readFileSync(file, 'utf8')
+        if (body.length === 0) {
+            continue
+        }
+
+        // polkadot apps paste replace method
+        const secBody = body.replaceAll("\n", ' ')
+        if (!isValidSignature(body, signature, owner) && !isValidSignature(secBody, signature, owner)) {
+            actions.setFailed("the signature is not valid")
+            verified = false
+        }
+
+        break
+    }
+
+    actions.setOutput('verified', verified ? 'true' : 'false')
+}
+
+main().then();
